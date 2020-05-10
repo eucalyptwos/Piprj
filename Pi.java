fedb5e1a33a84f5446f7dfcbebbcc3b243aa49d0
eucal <2468abcde@naver.com>
2020-05-10 11:16:56 +0900

    존윌리스공식

--- Pi.java
+++ Pi.java
@@ -1,21 +1,20 @@
 public class Pi{
 	public static void main(String[] args) {
 		int k=1;
-		double pie=0, pie_ori;
-		double sign=1.0, tolerr = 0.0000000025;
+		double pie=1.0, pie_ori;
+		double sign=1.0, tolerr = 0.00000000025;
 		double err=1.0;
 		while(true){
 			pie_ori=pie;
-			pie+=(1.0 / k)*sign;
-			sign = -sign;
-			k+=2;
+			pie*=1/(1-1/(4*Math.pow(k,2)));
+			k+=1;
 			err=pie_ori-pie;
 			if(err<0)
 				err= -err;
 			if(err<=tolerr)
 				break;
 			}
-		pie=pie*4;
+		pie=pie*2;
 		System.out.println("pie= "+pie);
 	}
 }
